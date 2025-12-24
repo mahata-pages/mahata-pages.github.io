0b8107af23212c84dcc8e7415223f8870937ba5d
Yasunori MAHATA <mahata777@gmail.com>
2025-12-24 23:57:45 +0900

    feat: add Header component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
+import { Header } from '@/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Vite + React</h1>
       <div>
         <p>
