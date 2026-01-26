cfd03fc88ac822c89aa2bd1307e9dfd2c3b1150e
Yasunori MAHATA <y@mahata.org>
2026-01-26 22:37:51 +0900

    feat: wrap logo in link to navigate to home (#32)

--- src/Header.ct.spec.tsx
+++ src/Header.ct.spec.tsx
@@ -1,8 +1,25 @@
 import { test, expect } from "@playwright/experimental-ct-react";
+import { MemoryRouter } from "react-router-dom";
+
 import { Header } from "@/Header";
 
 test("should show logo image in header", async ({ mount }) => {
-  const component = await mount(<Header />);
+  const component = await mount(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>,
+  );
 
   await expect(component.getByRole("img", { name: "Logo" })).toBeVisible();
+});
+
+test("logo link points to root", async ({ mount }) => {
+  const component = await mount(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>,
+  );
+
+  const logoLink = component.getByRole("link", { name: "Go to home" });
+  await expect(logoLink).toHaveAttribute("href", "/");
 });
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 import styles from "@/Header.module.css";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src="/logo.webp" alt="Logo" className={styles.logo} />
+      <Link to="/" aria-label="Go to home">
+        <img src="/logo.webp" alt="Logo" className={styles.logo} />
+      </Link>
     </header>
   );
 }
